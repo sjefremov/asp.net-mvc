b4059227f24bbe36dc681c66b5180c4566fe36db
Stojancho Jefremov <stojanco.jefremov@gmail.com>
2016-05-03 20:26:41 +0200

    First lesson finished

---
+++ SEDCCodeAcademy/MVCFirstProject/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Stojancho";
+            ViewBag.Color = "#999";
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            var people = new List<string> { "Pavle", "Stojancho", "Trajko", "Petko", "Mitra", "Vera", "Stojko" };
+            ViewBag.People = people;
+            return View();
+        }
+    }
+}
