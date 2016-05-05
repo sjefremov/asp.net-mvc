3807a1c8cc952bf4a1db2bf41ab3339b3cc78f49
Stojancho Jefremov <stojanco.jefremov@gmail.com>
2016-05-05 20:42:46 +0200

    SecondClassChangesAdded

--- CodeAcademy.ProjectManagament/CodeAcademy.ProjectManagament.WebApp/Controllers/ProjectsController.cs
+++ CodeAcademy.ProjectManagament/CodeAcademy.ProjectManagament.WebApp/Controllers/ProjectsController.cs
@@ -11,6 +11,38 @@ namespace CodeAcademy.ProjectManagament.WebApp.Controllers
         // GET: Projects
         public ActionResult Index()
         {
+            //List of all projects + favorited projects
+            //Important: Keep in mind longer list of projects might need pagination
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string name, string desc, int estimate)
+        {
+            ViewBag.Name = name;
+            ViewBag.Description = desc;
+            ViewBag.Estimate = estimate;
+            return View();
+        }
+        
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult Favorite(int id, bool favorite)
+        {
             return View();
         }
     }
---
+++ CodeAcademy.ProjectManagament/CodeAcademy.ProjectManagament.WebApp/Database/Projects.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAcademy.ProjectManagament.WebApp.Database
+{
+    public class Projects
+    {
+        public List<Project> GetAll()
+        {
+            return new List<Project>();
+        }
+
+        
+    }
+
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Estimate { get; set; }
+    }
+
+}
