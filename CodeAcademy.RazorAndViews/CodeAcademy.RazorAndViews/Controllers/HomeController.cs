86d9f76f1f3d7bb8a04faf99e1f13c474bc844cc
Stojancho Jefremov <stojanco.jefremov@gmail.com>
2016-05-07 13:11:27 +0200

    ExerciseFinished

---
+++ CodeAcademy.RazorAndViews/CodeAcademy.RazorAndViews/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeAcademy.RazorAndViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var texts = new List<string>
+            {
+                "Hello, right now I'm writing C# code for ASP.NET MVC.",
+                "This MVC seems fun",
+                "I like writing code in .NET Framework",
+                "Razor view engine seems awsome"
+            };
+
+            return View(texts);
+        }
+
+        public ActionResult Exercise()
+        {
+            var texts = new List<string>
+            {
+                "Hello, right now I'm writing C# code for ASP.NET MVC.",
+                "This MVC seems fun",
+                "I like writing code in .NET Framework",
+                "Razor view engine seems awsome",
+                "Hello, right now I'm writing C# code for ASP.NET MVC.",
+                "This MVC seems fun",
+                "I like writing code in .NET Framework",
+                "Razor view engine seems awsome",
+                "Hello, right now I'm writing C# code for ASP.NET MVC.",
+                "This MVC seems fun",
+                "I like writing code in .NET Framework",
+                "Razor view engine seems awsome"
+            };
+
+            return View(texts);
+        }
+
+
+    }
+}
---
+++ CodeAcademy.RazorAndViews/CodeAcademy.RazorAndViews/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeAcademy.RazorAndViews.MvcApplication" Language="C#" %>
