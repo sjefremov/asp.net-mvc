2385accd289e465f58541cb558f9eae69521926b
Стојанчо Јефремов < >
2016-05-12 00:00:37 +0200

    Homework Started

---
+++ EquipmentTracking/EquipmentTracking.WebApp/Models/Equipment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EquipmentTracking.WebApp.Models
+{
+    public class Equipment
+    {
+
+        public Equipment()
+        {
+            ID = Guid.NewGuid();
+        }
+
+        public Guid ID { get; private set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+    }
+}
