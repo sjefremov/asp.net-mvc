6b11c156f08a589c56c45b5a5c1437fb98bed4a4
Stojancho Jefremov <stojanco.jefremov@gmail.com>
2016-05-25 18:24:10 +0200

    Some changes added

--- CodeAcademy.TaskManagement.WebApp/CodeAcademy.TaskManagement.Domain.Entities/TaskComment.cs
+++ CodeAcademy.TaskManagement.WebApp/CodeAcademy.TaskManagement.Domain.Entities/TaskComment.cs
@@ -8,5 +8,16 @@ namespace CodeAcademy.TaskManagement.Domain.Entities
 {
     public class TaskComment
     {
+        public int ID { get; set; }
+
+        public string Comment { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual int UserId { get; set; }
+
+        public virtual Task Task { get; set; }
+
+        public virtual int TaskId { get; set; }
     }
 }
---
+++ CodeAcademy.TaskManagement.WebApp/CodeAcademy.TaskManagement.RepositoryEF/Repositories/TaskRepository.cs
@@ -0,0 +1,48 @@
+﻿using CodeAcademy.TaskManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace CodeAcademy.TaskManagement.RepositoryEF.Repositories
+{
+    public class TaskRepository
+    {
+        public List<Task> GetAll()
+        {
+
+            return new List<Task>();
+        }
+
+        public Task GetById(int id)
+        {
+            return new Task();
+        }
+
+        public List<TaskComment> GetComments(int taskId)
+        {
+            return new List<TaskComment>();
+        }
+
+        public string GetAssigneeName(int taskId)
+        {
+            return string.Empty;
+        }
+
+        public bool Create(Task task)
+        {
+            return false;
+        }
+
+        public bool Update(Task task)
+        {
+            return false;
+        }
+
+        public bool Delete(Task task)
+        {
+            return false;
+        }
+    }
+}
